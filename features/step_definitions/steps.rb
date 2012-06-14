5d7c74f9edc4a542e41545365086307763c837bf
Kendall Gifford <zettabyte@gmail.com>
2012-06-14 15:10:39 -0600

    Add default, pending step definitions

---
+++ features/step_definitions/steps.rb
@@ -0,0 +1,18 @@
+# encoding: utf-8
+
+Given /^a rack app that uses sessions$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given /^a redis server at "(.*?)"$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^I configure my rack app to use the redrack\-session middleware$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^my app's session data is stored in my redis database$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
